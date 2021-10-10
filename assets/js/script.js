00abab348a0a9e5f3aa577a908cb6058d86ffe9a
gabriel-perez1 <83041381+gabriel-perez1@users.noreply.github.com>
2021-10-10 11:27:33 -0400

    rectangular brush and variable diameter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 
-let xPos, yPos;
-let dia = 80;
+let xPos, yPos, dia;
 let R, G, B, A = 50;
 let brush1 = true;
+let brush2 = false;
+let brush3 = false;
 //initial canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,15 +15,27 @@ function draw() {
   //paramaters that are always changing need to be defined inside the function
   xPos = mouseX;
   yPos = mouseY;
+  dia = map(mouseY, 0, height, 0, 80); //remaps diameter based on mouse position in y-axis
   //map(value, start1, stop1, start2, stop2);
   noStroke();
-  //remaps RGB values in respect to mouse position
+  //remaps RGBA values based on mouse position
   R = map(mouseX, 0, width, 0, 255);
   G = map(mouseY, 0, height, 0, 150);
   B = map(mouseY, 0, width, 0, 255);
+  A = map(mouseX, 0, width, 0, 255);
+  //if statements for keyboard inputs
   if(brush1){
-  fill(R, G, B, A); //if using rgba, 0 will make it transparent and 255 completely solid
-  circle(xPos, yPos, dia);
+    fill(R, G, B, A); //if using rgba, 0 will make it transparent and 255 completely solid
+    circle(xPos, yPos, dia);
+  }
+  if(brush2){
+    fill('lightpink');
+    circle(xPos, yPos, dia);
+  }
+  if(brush3){
+    fill('white');
+    stroke('red');
+    rect(xPos, yPos, dia, dia);
   }
 }
 
@@ -30,5 +43,11 @@ function draw() {
 function keyTyped(){
   if (key === '1') {
     brush1 = !brush1; //will turn brush on/off with keyboard input '1'
+  }
+  if (key ==='2') {
+    brush2 = !brush2; //will make brush pink with keyboard input '2'
+  }
+  if (key ==='3') {
+    brush3 = !brush3; //will make a rectangular white1 brush with keyboard input '3'
   }
 }
