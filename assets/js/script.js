941bf3f93906118f0403a67c3e39ad87e6ff7890
gabriel-perez1 <83041381+gabriel-perez1@users.noreply.github.com>
2021-10-10 11:00:53 -0400

    remaps rgb values with mouse position

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 let xPos, yPos;
 let dia = 80;
-
+let R, G, B, A = 50;
 //initial canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,8 +10,15 @@ function setup() {
 
 //continously updates canvas
 function draw() {
+  //paramaters that are always changing need to be defined inside the function
   xPos = mouseX;
   yPos = mouseY;
-  fill('lightpink');
+  //map(value, start1, stop1, start2, stop2);
+  noStroke();
+  //remaps RGB values in respect to mouse position
+  R = map(mouseX, 0, width, 0, 255);
+  G = map(mouseY, 0, height, 0, 150);
+  B = map(mouseY, 0, width, 0, 255);
+  fill(R, G, B, A); //if using rgba, 0 will make it transparent and 255 completely solid
   circle(xPos, yPos, dia);
 }
